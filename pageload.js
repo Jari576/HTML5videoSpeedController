44f7b19cc2d93a51979894226ff41d0e19eaa97d
jari <jaribervoets@gmail.com>
2021-10-22 05:40:27 +0200

    page navigation

--- pageload.js
+++ pageload.js
@@ -1,3 +1,10 @@
-window.onload = function () {
-    console.log("page load!");
-};
+const observer = new MutationObserver(mutations => {
+    chrome.storage.local.get({ speed: 1 }, response => {
+        [...document.getElementsByTagNameNS("http://www.w3.org/1999/xhtml", "video")].forEach(video => video.playbackRate = response.speed);
+    });
+});
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true
+});
